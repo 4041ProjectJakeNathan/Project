b157d4d8d90a14e734c970bf081b15cd09e5144e
NathanStudanski <studa003@umn.edu>
2014-11-03 20:40:30 -0600

    Added Sobel

--- Pixels.java
+++ Pixels.java
@@ -22,16 +22,36 @@ public class Pixels {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String fileName = "skyline.jpg";
+        String fileName = "landscape.jpg";
         BufferedImage img = readImage(fileName);
         System.out.println("Width = " + img.getWidth());
         System.out.println("Height = " + img.getHeight());
         ColorPixel[][] pixels = filterAndArray(img);
+        BufferedImage sobel = sobelOperator(pixels);
+        grayToFakeColor(sobel);
+        
         //printLumiGrayArray(pixels);
         //buildImage(pixels);
         //test();
     }
+    protected static void grayToFakeColor(BufferedImage img){
+        BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        int c;
+        for (int i = 0; i < img.getWidth(); i++) {
+            for (int j = 0; j < img.getHeight(); j++) {
+               c = img.getRGB(i, j);
+               result.setRGB(i, j, c);
+            }
+        }
+        createFileImage(result, "sobelContrast");
+    }
     
+    protected static void createFileImage(BufferedImage img, String fileName){
+        try{
+        ImageIO.write(img, "jpg", new File(fileName + ".jpg"));
+        }
+        catch(IOException e) {System.out.println("IO Exception");}
+    }
     protected static BufferedImage readImage(String fileName) {
         try{File file = new File(fileName); 
         return ImageIO.read(file);}
@@ -55,14 +75,10 @@ public class Pixels {
                 sepia.setRGB(i, j, c.getRGB());
             }
         }
-        
-        try{
-        ImageIO.write(avggray, "jpg", new File("averageGray.jpg"));
-        ImageIO.write(lumigray, "jpg", new File("lumiGray.jpg"));
-        ImageIO.write(lightgray, "jpg", new File("lightGray.jpg"));
-        ImageIO.write(sepia, "jpg", new File("sepia.jpg"));
-        }
-        catch(IOException e) {System.out.println("IO Exception");}
+        createFileImage(avggray, "averageGray");
+        createFileImage(lumigray, "lumiGray");
+        createFileImage(lightgray, "lightGray");
+        createFileImage(sepia, "sepia");
         return ans;
     }
     protected static void test(){
@@ -86,7 +102,62 @@ public class Pixels {
         }
         System.out.println(s);
     }
-    
+    public int[][] addBuffer(ColorPixel[][] greyImage)
+	{
+		/*
+		 * This function's purpose is not necessary, but it adds a border to the original
+		 * greyscale image by copying the image's current border.  This is meant to preserve
+		 * the original size of the image as a matrix is applied.
+		 */
+		int[][] bufferedImage = new int[greyImage.length+2][greyImage[0].length+2];
+		for (int x = 0; x < greyImage.length; x++)
+			for (int y = 0; y < greyImage[0].length; y++)
+				bufferedImage[x+1][y+1] = greyImage[x][y].getLumiGray();
+		bufferedImage[0] = bufferedImage[1]; //handles first row
+		bufferedImage[bufferedImage.length-1] = bufferedImage[bufferedImage.length-2]; //handles last row
+		bufferedImage[0][0] = bufferedImage[0][1]; //handles first column
+		bufferedImage[bufferedImage.length-1][bufferedImage[0].length-1] = bufferedImage[bufferedImage.length-2][bufferedImage[0].length-2]; //handles last column
+		
+				
+		bufferedImage[0][0] = greyImage[0][0].getLumiGray(); //top left
+		bufferedImage[0][bufferedImage[0].length-1] = greyImage[0][greyImage[0].length-1].getLumiGray(); //top right
+		bufferedImage[bufferedImage.length-1][0] = greyImage[greyImage.length-1][0].getLumiGray(); //bottom left
+		bufferedImage[bufferedImage.length-1][bufferedImage[0].length-1] = greyImage[greyImage.length-1][greyImage[0].length-1].getLumiGray(); //bottom right
+	
+		return bufferedImage;
+	}
+	
+	public static BufferedImage sobelOperator(ColorPixel[][] b_img)
+	{
+		int[][] xSobelFilter = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}}; //filters for x gradient
+		int[][] ySobelFilter = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}}; //filters for y gradient
+		
+		BufferedImage result = new BufferedImage(b_img.length, b_img[0].length, BufferedImage.TYPE_BYTE_GRAY); //will store image gradient
+		
+		//applies filters to get final gradient image
+		for (int x = 0; x < b_img.length; x++)
+		{	//how to get height and width?
+			for(int y = 0; y < b_img[0].length; y++)
+			{
+                                if(x == 0 || y == 0 || x == (b_img.length - 1) || y == (b_img[0].length - 1)){ result.setRGB(x, y, b_img[x][y].getLumiGray());}
+                                else{int newX = xSobelFilter[0][0]*b_img[x-1][y-1].getLumiGray() + xSobelFilter[0][1]*b_img[x-1][y].getLumiGray() + xSobelFilter[0][2]*b_img[x-1][y+1].getLumiGray() +
+						xSobelFilter[1][0]*b_img[x-1][y].getLumiGray() + xSobelFilter[1][1]*b_img[x][y].getLumiGray() + xSobelFilter[1][2]*b_img[x+1][y-1].getLumiGray() +
+						xSobelFilter[2][0]*b_img[x-1][y+1].getLumiGray() + xSobelFilter[2][1]*b_img[x][y+1].getLumiGray() + xSobelFilter[2][2]*b_img[x+1][y+1].getLumiGray();
+				
+				int newY = ySobelFilter[0][0]*b_img[x-1][y-1].getLumiGray() + ySobelFilter[0][1]*b_img[x-1][y].getLumiGray() + ySobelFilter[0][2]*b_img[x-1][y+1].getLumiGray() +
+						ySobelFilter[1][0]*b_img[x-1][y].getLumiGray() + ySobelFilter[1][1]*b_img[x][y].getLumiGray() + ySobelFilter[1][2]*b_img[x+1][y-1].getLumiGray() +
+						ySobelFilter[2][0]*b_img[x-1][y+1].getLumiGray() + ySobelFilter[2][1]*b_img[x][y+1].getLumiGray() + ySobelFilter[2][2]*b_img[x+1][y+1].getLumiGray();
+				
+				int gradient = (int) Math.sqrt(newX*newX + newY*newY);
+				result.setRGB(x, y, gradient);  //good for left borders, but right?
+                                }
+			}
+		}
+                createFileImage(result, "sobel");
+                return result;
+	}
+
+
     protected static void buildImage(ColorPixel[][] a){
         ColorPixel p;
         BufferedImage img = new BufferedImage(a.length, a[0].length, BufferedImage.TYPE_BYTE_GRAY);
@@ -101,4 +172,6 @@ public class Pixels {
         }
         catch(IOException e) {System.out.println("IO Exception");}
     }
+    
+    
 }

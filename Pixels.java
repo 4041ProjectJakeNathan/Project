98e1d682d92325f9ccedec8464117f783c2ad3bd
NathanStudanski <studa003@umn.edu>
2014-11-03 23:14:09 -0600

    Update Pixels.java

--- Pixels.java
+++ Pixels.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.Color;
+import java.io.FileWriter;
 /**
  *
  * @author Nathan Studanski
@@ -22,19 +23,57 @@ public class Pixels {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String fileName = "landscape.jpg";
+        String fileName = "skyline.jpg";
         BufferedImage img = readImage(fileName);
         System.out.println("Width = " + img.getWidth());
         System.out.println("Height = " + img.getHeight());
         ColorPixel[][] pixels = filterAndArray(img);
         BufferedImage sobel = sobelOperator(pixels);
-        grayToFakeColor(sobel);
+        grayToFakeColor(sobel, "sobelContrast");
+        BufferedImage roberts = robertsOperator(pixels);
+        grayToFakeColor(roberts, "robertsContrast");
+        ColorPixel[][] robertsArray = filterAndArray(roberts);
+        ColorPixel[][] sobelArray = filterAndArray(sobel);
+        BufferedImage sobelRob = sobelOperator(robertsArray);
+        grayToFakeColor(sobelRob, "sobelRobertsContrast");
+        
         
         //printLumiGrayArray(pixels);
         //buildImage(pixels);
         //test();
     }
-    protected static void grayToFakeColor(BufferedImage img){
+    protected static void arrayToTxt(ColorPixel[][] arr, String fileName){
+        int gray;
+        String fGray;
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                /*gray = arr[i][j].getLumiGray();
+                if(gray < 10){ fGray = "00" + gray;}
+                else if (gray < 100 & gray > 9){ fGray = "0" + gray;}
+                else{fGray = "" + gray;}
+                result += fGray + " ";
+                */
+                gray = arr[i][j].getLumiGray();
+                if(gray > 30){fGray = "X";}
+                else{fGray = " ";}
+                result += fGray;
+            }
+            result += System.lineSeparator();
+            
+        }
+        try {
+            File newTextFile = new File(fileName + ".txt");
+
+            FileWriter fw = new FileWriter(newTextFile);
+            fw.write(result);
+            fw.close();
+
+        } catch (IOException e) {
+            
+        }
+    }
+    protected static void grayToFakeColor(BufferedImage img, String fileName){
         BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
         int c;
         for (int i = 0; i < img.getWidth(); i++) {
@@ -43,7 +82,7 @@ public class Pixels {
                result.setRGB(i, j, c);
             }
         }
-        createFileImage(result, "sobelContrast");
+        createFileImage(result, fileName);
     }
     
     protected static void createFileImage(BufferedImage img, String fileName){
@@ -59,26 +98,32 @@ public class Pixels {
     }
     
     protected static ColorPixel[][] filterAndArray (BufferedImage img){
+        /*
         BufferedImage avggray = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
         BufferedImage lumigray = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
         BufferedImage lightgray = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
         BufferedImage sepia = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        */
         ColorPixel[][] ans = new ColorPixel[img.getWidth()][img.getHeight()];
         Color c;
         for (int i = 0; i < img.getWidth(); i++) {
             for (int j = 0; j < img.getHeight(); j++) {
                 ans[i][j] = new ColorPixel(img.getRGB(i, j));
+                /*
                 lumigray.setRGB(i, j, ans[i][j].getLumiGray());
                 avggray.setRGB(i, j, ans[i][j].getAverageGray());
                 lightgray.setRGB(i, j, ans[i][j].getLightGray());
                 c = new Color(ans[i][j].getSepiaRed(), ans[i][j].getSepiaGreen(), ans[i][j].getSepiaBlue());
                 sepia.setRGB(i, j, c.getRGB());
+                */
             }
         }
+        /*
         createFileImage(avggray, "averageGray");
         createFileImage(lumigray, "lumiGray");
         createFileImage(lightgray, "lightGray");
         createFileImage(sepia, "sepia");
+        */
         return ans;
     }
     protected static void test(){
@@ -102,13 +147,13 @@ public class Pixels {
         }
         System.out.println(s);
     }
-    public int[][] addBuffer(ColorPixel[][] greyImage)
+    /*public int[][] addBuffer(ColorPixel[][] greyImage)
 	{
-		/*
+		*
 		 * This function's purpose is not necessary, but it adds a border to the original
 		 * greyscale image by copying the image's current border.  This is meant to preserve
 		 * the original size of the image as a matrix is applied.
-		 */
+		 
 		int[][] bufferedImage = new int[greyImage.length+2][greyImage[0].length+2];
 		for (int x = 0; x < greyImage.length; x++)
 			for (int y = 0; y < greyImage[0].length; y++)
@@ -125,8 +170,25 @@ public class Pixels {
 		bufferedImage[bufferedImage.length-1][bufferedImage[0].length-1] = greyImage[greyImage.length-1][greyImage[0].length-1].getLumiGray(); //bottom right
 	
 		return bufferedImage;
-	}
-	
+	} 
+    */
+	public static BufferedImage robertsOperator(ColorPixel[][] img){
+            BufferedImage result = new BufferedImage(img.length, img[0].length, BufferedImage.TYPE_BYTE_GRAY);
+            int grad, a, b;
+            for (int i = 0; i < img.length; i++) {
+                for (int j = 0; j < img[0].length; j++) {
+                    if(i == (img.length - 1) || j == (img[0].length - 1)){result.setRGB(i, j, img[i][j].getLumiGray());}
+                    else{
+                        a = img[i][j].getLumiGray() - img[i + 1][j+1].getLumiGray();
+                        b = img[i][j + 1].getLumiGray() - img[i+1][j].getLumiGray();
+                        grad = a * a + b * b;
+                        result.setRGB(i, j, grad);
+                    }
+                }
+            }
+            createFileImage(result, "roberts");
+            return result;
+        }
 	public static BufferedImage sobelOperator(ColorPixel[][] b_img)
 	{
 		int[][] xSobelFilter = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}}; //filters for x gradient
@@ -149,6 +211,8 @@ public class Pixels {
 						ySobelFilter[2][0]*b_img[x-1][y+1].getLumiGray() + ySobelFilter[2][1]*b_img[x][y+1].getLumiGray() + ySobelFilter[2][2]*b_img[x+1][y+1].getLumiGray();
 				
 				int gradient = (int) Math.sqrt(newX*newX + newY*newY);
+                                if (gradient > 120){gradient = 255;}
+                                else{gradient = 0;}
 				result.setRGB(x, y, gradient);  //good for left borders, but right?
                                 }
 			}
